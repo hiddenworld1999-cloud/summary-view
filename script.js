a9238d93f281f84c35d25229b6127330573780e1
hiddenworld1999-cloud <hiddenworld1999@gmail.com>
2025-12-30 11:41:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// منع كليك يمين وتحديد النص
+document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("selectstart", e => e.preventDefault());
+document.addEventListener("keydown", e => {
+  if (e.ctrlKey && ['c','s','p','u'].includes(e.key.toLowerCase())) {
+    e.preventDefault();
+  }
+});
+
+// قراءة الاسم من اللينك
+const params = new URLSearchParams(window.location.search);
+const student = params.get("name") || "طالب غير معروف";
+ 
+// Watermark مرة واحدة في النص
+const wm = document.getElementById("watermark");
+wm.innerText = "خاص للطالب " + student;

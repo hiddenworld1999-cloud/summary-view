ad751b71fd50cc36835db8906f65d25138d94e69
hiddenworld1999-cloud <hiddenworld1999@gmail.com>
2025-12-30 21:52:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// منع كليك يمين وتحديد النص ونسخ الصفحة
-document.addEventListener("contextmenu", e => e.preventDefault());
-document.addEventListener("selectstart", e => e.preventDefault());
-document.addEventListener("keydown", e => {
-  if (e.ctrlKey && ['c','s','p','u'].includes(e.key.toLowerCase())) {
-    e.preventDefault();
-  }
-});
-
 const student = sessionStorage.getItem("studentName");
 if (!student) {
   window.location.href = "login.html";
 }
 
 const text = "خاص بالطالب\n" + student;
-
 document.getElementById("watermark-top").innerText = text;
 document.getElementById("watermark-bottom").innerText = text;

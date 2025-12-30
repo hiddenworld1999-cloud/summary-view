217e127ad88f1685c8151f2c9e9b83c82dfc7318
hiddenworld1999-cloud <hiddenworld1999@gmail.com>
2025-12-30 12:21:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 document.addEventListener("contextmenu", e => e.preventDefault());
 document.addEventListener("selectstart", e => e.preventDefault());
 document.addEventListener("keydown", e => {
-  if (e.ctrlKey && ['c','s','p','u'].includes(e.key.toLowerCase())) {
-    e.preventDefault();
-  }
+  if (e.ctrlKey && ['c','s','p','u'].includes(e.key.toLowerCase())) e.preventDefault();
 });
 
 // قراءة الاسم من الرابط
 const params = new URLSearchParams(window.location.search);
 const student = params.get("name");
 
-// لو مفيش اسم → ارجع للصفحة الرئيسية
+// حماية من فتح index مباشرة
 if(!student) {
+  // خزّن العلم في sessionStorage قبل إعادة التوجيه
+  sessionStorage.setItem('redirected', 'true');
   window.location.href = "login.html";
+} else {
+  // تحقق من sessionStorage لو تم التحويل مسبقًا
+  sessionStorage.removeItem('redirected');
 }
 
 // Watermark
